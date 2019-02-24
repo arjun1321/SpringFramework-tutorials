2d83f3b4d63a3c9b5a24d02d44b309632fbd62c9
arjun1321 <guptaarjun152@gmail.com>
2019-02-24 20:35:18 +0530

    Hibernate DAO

--- web-customer-tracker/src/com/arjunkumar/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/com/arjunkumar/springdemo/controller/CustomerController.java
@@ -1,8 +1,14 @@
 package com.arjunkumar.springdemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.arjunkumar.springdemo.dao.CustomerDAO;
+import com.arjunkumar.springdemo.entity.Customer;
 
 
 
@@ -10,8 +16,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/customer")
 public class CustomerController {
 	
+	// need to inject customer dao
+	@Autowired
+	private CustomerDAO customerDAO;
+	
 	@RequestMapping("/list")
 	public String listCustomer(Model model) {	
+		
+		// get customers from dao
+		List<Customer> theCustomers = customerDAO.getCustomers();
+		
+		// add customers to the model
+		model.addAttribute("customers", theCustomers);
+		
 		return "list-customer";
 	}
 
---
+++ web-customer-tracker/src/com/arjunkumar/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,10 @@
+package com.arjunkumar.springdemo.dao;
+
+import java.util.List;
+
+import com.arjunkumar.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+	
+	public List<Customer> getCustomers();
+}
