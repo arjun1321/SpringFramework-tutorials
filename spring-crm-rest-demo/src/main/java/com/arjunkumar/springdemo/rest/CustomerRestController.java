a75c958dfcfad5937cadab096c9eed16fe86ea49
arjun1321 <guptaarjun152@gmail.com>
2019-04-14 14:08:06 +0530

    Spring REST: CRUD Database realtime project getCustomers()

---
+++ spring-crm-rest-demo/src/main/java/com/arjunkumar/springdemo/rest/CustomerRestController.java
@@ -0,0 +1,34 @@
+package com.arjunkumar.springdemo.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.arjunkumar.springdemo.entity.Customer;
+import com.arjunkumar.springdemo.service.CustomerService;
+
+@RestController
+@RequestMapping("/api")
+public class CustomerRestController {
+	
+	// autowire the customer service
+	@Autowired
+	private CustomerService customerService;
+	
+	// add mappinng for GET/customers
+	@GetMapping("/customers")
+	public List<Customer> getCustomers() {
+		return customerService.getCustomers();
+	}
+	
+	// add mapping for GET/customers/{customerId}
+	@GetMapping("/customers/{customerId}")
+	public Customer getCustomer(@PathVariable int customerId) {
+		return customerService.getCustomer(customerId);
+	}
+
+}
